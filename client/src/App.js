ac40bdfe5feb8252a8d7dba444631ec9fb9406cf
pranavpeter2050 <97501797+pranavpeter2050@users.noreply.github.com>
2022-09-19 17:34:51 +0530

    Added input & button to enter & send message.

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,14 @@
 import './App.css';
 
 function App() {
+
+  const sendMessage = () => {
+
+  };
   return (
     <div className="App">
-      
+      <input placeholder="Message..."/>
+      <button onClick={sendMessage}>Send Message</button>
     </div>
   );
 }
