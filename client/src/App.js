12d58eeb7103330c4a8569891077b2d0b4bef729
pranavpeter2050 <97501797+pranavpeter2050@users.noreply.github.com>
2022-09-19 18:59:22 +0530

    created events to send message to server and receive message from server.

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,35 @@
 import './App.css';
+import io from 'socket.io-client';
+import { useEffect, useState } from 'react'; 
+
+const socket = io.connect("http://localhost:3001");
 
 function App() {
 
+  const [message, setMessage] = useState("");
+  const [messageReceived, setMessageReceived] = useState("");
   const sendMessage = () => {
-
+    socket.emit("send_message", { message });
   };
+
+  useEffect(() => {
+    socket.on("receive_message", (data) => {
+      // alert(data.message);
+      setMessageReceived(data.message);
+    });
+  });
+
   return (
     <div className="App">
-      <input placeholder="Message..."/>
+      <input 
+        placeholder="Message..." 
+        onChange={(event) => {
+          setMessage(event.target.value);
+        }} 
+      />
       <button onClick={sendMessage}>Send Message</button>
+      <h1>Message:</h1>
+      { messageReceived }
     </div>
   );
 }
